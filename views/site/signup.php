3e41bca9ec98b67dde026fccf182d0f2bd29e55b
Emerald Zhang <ejzhang2004@gmail.com>
2021-08-15 12:26:34 -0500

    delete login breadcrumb

--- views/site/signup.php
+++ views/site/signup.php
@@ -9,13 +9,12 @@ use yii\bootstrap\Button;
 /* @var $form yii\widgets\ActiveForm */
 
 $this->title = 'Sign Up';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
 
 <div class="signup">
 <p>
-    <?= Html::a('Sign up as Coach', ['/site/signup_coach'], ['class'=>'btn btn-primary']) ?>
-    <?= Html::a('Sign up as Swimmer', ['/site/signup_swimmer'], ['class'=>'btn btn-primary']) ?>
+    <?= Html::a('Sign up as a Coach', ['/site/signup_coach'], ['class'=>'btn btn-primary']) ?>
+    <?= Html::a('Sign up as a Swimmer', ['/site/signup_swimmer'], ['class'=>'btn btn-primary']) ?>
 
     </p>
 
